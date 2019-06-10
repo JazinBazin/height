b9e3c4e8618dcaf11004d04fb3e20ce50e835577
jazinbazin <yudenichaa@yandex.ru>
2019-06-10 22:08:37 +0300

    land name fix

--- agency/models/land.py
+++ agency/models/land.py
@@ -3,7 +3,7 @@ from .real_estate import RealEstate
 
 class Land(RealEstate):
     class Meta:
-        verbose_name = 'земелный участок'
+        verbose_name = 'земельный участок'
         verbose_name_plural = 'Земельные участки'
 
     description_page = 'land_description'
